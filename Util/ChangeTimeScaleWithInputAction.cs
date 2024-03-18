7f0e2cab3e83bb24736b3819f9873946080a3eb3
Erkberg <erisch01@web.de>
2024-03-18 10:34:26 +0100

    ChangeTimeScaleWithInputAction util class

---
+++ Util/ChangeTimeScaleWithInputAction.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class ChangeTimeScaleWithInputAction : Node
+{
+    [Export] private string actionName;
+    [Export] private float changedTimeScale = 8f;
+    [Export] private float regularTimeScale = 1f;
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed(actionName))
+        {
+            Engine.TimeScale = changedTimeScale;
+        }
+        else if (Input.IsActionJustReleased(actionName))
+        {
+            Engine.TimeScale = regularTimeScale;
+        }
+    }
+}
