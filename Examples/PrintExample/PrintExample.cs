891d5fef565170ac7f5b8c597bb81d25bfa9add0
Erkberg <erisch01@web.de>
2024-04-11 14:18:58 +0200

    print example

---
+++ Examples/PrintExample/PrintExample.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class PrintExample : Node3D
+{
+    public override void _Ready()
+    {
+        // Simple
+        GD.Print("Hi there!");
+        GD.Print(Vector3.Up);
+        GD.Print(Colors.Chocolate);
+        GD.Print(this);
+
+        // Concatenations
+        GD.Print("My name is " + Name);
+        GD.Print($"My position is {GlobalPosition}");
+
+        GD.PrintT("PrintT", Position, Rotation, Scale);
+        GD.PrintS("PrintS", Position, Rotation, Scale);
+
+        // Rich text
+        GD.PrintRich("[b]Bold[/b] ", "[i]Italic[/i] ", "[b][i]Bold Italic[/i][/b]");
+        // more examples
+
+        // Warnings & Errors
+    }
+}
